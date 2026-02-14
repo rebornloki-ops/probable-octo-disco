86dee5f8761cb01052502e408b93dd2ab8574ce4
Rebornloki <lydieakokpe@gmail.com>
2026-02-14 22:16:43 +0100

    Learned tuples

--- intro.py
+++ intro.py
@@ -1,4 +1,8 @@
-list_1= ['History','Math','Physics','CompSci']
-list_2 = list_1
-print(list_1)
-print(list_2)
+#Imutable
+tuple_1= ('History','Math','Physics','CompSci')
+tuple_2= tuple_1
+print(tuple_1)
+print(tuple_2)
+tuple_1[0]='Art' 
+print(tuple_1)
+print(tuple_2)
